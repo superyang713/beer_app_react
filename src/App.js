70ebd46b14e6fc990bb21e3e6514a178ab323c76
Yang Dai <yang@Yangs-MacBook-Pro.local>
2019-02-02 00:58:15 -0500

    The app is working. Need to polish a little bit

--- src/App.js
+++ src/App.js
@@ -6,17 +6,33 @@ import Form from "./component/Form/Form.js";
 import "./App.css";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      beer: beerData.beerData,
-    };
-  }
+  state = {
+    beer: [],
+    result: {},
+  };
 
   getBeer = e => {
     e.preventDefault();
-    console.log("hello");
+    const submitData = {
+      OG: e.target.elements.OG.value,
+      FG: e.target.elements.FG.value,
+      IBU: e.target.elements.IBU.value,
+      SRM: e.target.elements.SRM.value,
+      ABV: e.target.elements.ABV.value,
+    };
+    const result = this.state.beer.filter(item => (
+      submitData.OG >= item.OG_min && submitData.OG <= item.OG_max &&
+        submitData.FG >= item.FG_min && submitData.FG <= item.FG_max &&
+        submitData.IBU >= item.IBU_min && submitData.IBU <= item.IBU_max &&
+        submitData.SRM >= item.SRM_min && submitData.SRM <= item.SRM_max &&
+        submitData.ABV >= item.ABV_min && submitData.ABV <= item.ABV_max 
+    ));
+    this.setState({result: result[0]});
   };
+
+  componentDidMount = () => {
+    this.setState({beer: beerData.beerData});
+  }
 
   render() {
     return (
@@ -24,6 +40,7 @@ class App extends Component {
         <Header />
         <Logo />
         <Form getBeer={this.getBeer} />
+        {this.state.result ? <h1>{this.state.result.name}</h1> : <h1>No result</h1>}
       </div>
     );
   }
